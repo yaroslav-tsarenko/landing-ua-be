36e9a4c8aa20ec43e24582b557e126b22e193bbb
Yaroslav <yaroslav7v@gmail.com>
2025-03-31 20:52:15 +0300

    new changes

--- controllers/request.controller.js
+++ controllers/request.controller.js
@@ -1,21 +1,44 @@
 const axios = require('axios');
 
 const sendMessageToKommo = async (req, res) => {
-
-    const { username, text, phone, nickname } = req.body;
+    const {
+        username,
+        phone,
+        email,
+        ip,
+        utm_source,
+        utm_medium,
+        utm_campaign,
+        userAgent,
+        device
+    } = req.body;
 
     try {
-        await axios.post(`https://tresortech.kommo.com/api/v4/leads`, [
+        await axios.post('https://tresortech.kommo.com/api/v4/leads', [
             {
-                name: `${text} ${username} (${nickname}) з телефоном ${phone}`,
-                contacts: [
-                    {
-                        first_name: username,
-                        custom_fields_values: [
-                            { field_name: "Повідомлення", values: [{ value: text }] }
-                        ]
-                    }
-                ]
+                name: "Лід із форми",
+                custom_fields_values: [
+                    { field_name: "Ім’я", values: [{ value: username }] },
+                    { field_name: "Телефон", values: [{ value: phone }] },
+                    { field_name: "Email", values: [{ value: email }] },
+                    { field_name: "IP", values: [{ value: ip }] },
+                    { field_name: "UTM Source", values: [{ value: utm_source }] },
+                    { field_name: "UTM Medium", values: [{ value: utm_medium }] },
+                    { field_name: "UTM Campaign", values: [{ value: utm_campaign }] },
+                    { field_name: "User-Agent", values: [{ value: userAgent }] },
+                    { field_name: "Пристрiй", values: [{ value: device }] }
+                ],
+                _embedded: {
+                    contacts: [
+                        {
+                            first_name: username,
+                            custom_fields_values: [
+                                { field_name: "Телефон", values: [{ value: phone }] },
+                                { field_name: "Email", values: [{ value: email }] }
+                            ]
+                        }
+                    ]
+                }
             }
         ], {
             headers: {
@@ -25,11 +48,9 @@ const sendMessageToKommo = async (req, res) => {
         });
         res.status(200).send('Message sent successfully');
     } catch (error) {
-        console.error("❌ Помилка при відправці в Kommo:", error);
+        console.error("❌ Помилка при вiдправцi в Kommo:", error);
         res.status(500).send('Error sending message');
     }
 };
 
-module.exports = {
-    sendMessageToKommo
-}
+module.exports = { sendMessageToKommo };
