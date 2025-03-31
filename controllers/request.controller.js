dd0e5f29db7b10721442f45876d112d55fdb42a4
Yaroslav <yaroslav7v@gmail.com>
2025-03-31 22:38:14 +0300

    new changes

--- controllers/request.controller.js
+++ controllers/request.controller.js
@@ -16,25 +16,25 @@ const sendMessageToKommo = async (req, res) => {
     try {
         await axios.post('https://tresortech.kommo.com/api/v4/leads', [
             {
-                name: "Лід із форми",
+                name: "Лід із форми LandingUA",
                 custom_fields_values: [
-                    { field_name: "Ім’я", values: [{ value: username }] },
-                    { field_name: "Телефон", values: [{ value: phone }] },
-                    { field_name: "Email", values: [{ value: email }] },
-                    { field_name: "IP", values: [{ value: ip }] },
-                    { field_name: "UTM Source", values: [{ value: utm_source }] },
-                    { field_name: "UTM Medium", values: [{ value: utm_medium }] },
-                    { field_name: "UTM Campaign", values: [{ value: utm_campaign }] },
-                    { field_name: "User-Agent", values: [{ value: userAgent }] },
-                    { field_name: "Пристрiй", values: [{ value: device }] }
+                    { field_id: 1051004, values: [{ value: username }] },
+                    { field_id: 1051006, values: [{ value: phone }] },
+                    { field_id: 1051008, values: [{ value: email }] },
+                    { field_id: 1051012, values: [{ value: ip }] },
+                    { field_id: 1051014, values: [{ value: utm_source }] },
+                    { field_id: 1051016, values: [{ value: utm_medium }] },
+                    { field_id: 1051018, values: [{ value: utm_campaign }] },
+                    { field_id: 1051022, values: [{ value: device }] },
+                    { field_id: 1051020, values: [{ value: userAgent }] }
                 ],
                 _embedded: {
                     contacts: [
                         {
                             first_name: username,
                             custom_fields_values: [
-                                { field_name: "Телефон", values: [{ value: phone }] },
-                                { field_name: "Email", values: [{ value: email }] }
+                                { field_id: 1051006, values: [{ value: phone }] },
+                                { field_id: 1051008, values: [{ value: email }] }
                             ]
                         }
                     ]
@@ -46,9 +46,10 @@ const sendMessageToKommo = async (req, res) => {
                 "Content-Type": "application/json"
             }
         });
+
         res.status(200).send('Message sent successfully');
     } catch (error) {
-        console.error("❌ Помилка при вiдправцi в Kommo:", error);
+        console.error("❌ Помилка при відправці в Kommo:", error?.response?.data || error.message);
         res.status(500).send('Error sending message');
     }
 };
